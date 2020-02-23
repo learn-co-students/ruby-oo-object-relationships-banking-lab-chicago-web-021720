bb89d7df5473d7f8fd573f37f84c0d960f030bf2
ellisandrews <ellisandrews1@gmail.com>
2020-02-23 11:14:00 -0600

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,31 @@
 class BankAccount
 
+    attr_reader :name
+    attr_accessor :balance, :status
+
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = 'open'
+    end
+
+    def deposit(amount)
+        # QUESTION: Given that I have `attr_accessor :balance`, why doesn't this work>?
+        #    balance += amount
+        self.balance += amount
+    end
+
+    def display_balance
+        "Your balance is $#{balance}."
+    end
+
+    def valid?
+        balance > 0 && status == 'open'
+    end
+
+    def close_account
+        # Why do we need `self.` here?
+        self.status = 'closed'
+    end
+    
 end
